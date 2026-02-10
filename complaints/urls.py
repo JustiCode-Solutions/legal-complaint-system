5246ef7eb8a8ff239883aa8c26ca596de3685829
Smriti Shaw <154730353+smritishaw123@users.noreply.github.com>
2026-02-10 22:18:48 +0530

    Add citizen portal route to urlpatterns

--- complaints/urls.py
+++ complaints/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'), 
     path('add/', views.add_complaint, name='add_complaint'),
+    path('citizen/', views.add_complaint, name='citizen_portal'),
 ]
